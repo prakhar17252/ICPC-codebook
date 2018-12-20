60b32c6b1f1d9083ea419e7d3defa0129adea856
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-20 22:29:07 +0530

    Added Java Template

---
+++ javatemplate.java
@@ -0,0 +1,27 @@
+import java.util.*; import java.lang.*;
+import java.io.*;
+
+public class Main{
+	public static void solve(PrintWriter out) 
+														throws Exception{}
+
+ 	public static void main(String[] args) 
+														throws Exception{
+ PrintWriter out = new PrintWriter(System.out);
+ 		solve(out); out.close();
+ 	}
+
+	static class in{
+	 static BufferedReader b = new BufferedReader
+				(new InputStreamReader(System.in));
+	 static StringTokenizer t = 
+											new StringTokenizer("");
+		static String next() throws Exception{
+			while (!t.hasMoreTokens())
+				t = new StringTokenizer(b.readLine());
+			return t.nextToken();
+		} static int nextInt() throws Exception{
+			return Integer.parseInt(next());
+		}	// Same for Long and Double
+	}
+}
