b315acb249da427addc140d51b6d75fa3b5d0320
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-20 21:42:56 +0530

    Added Dijkstra

---
+++ dijkstra.cpp
@@ -0,0 +1,16 @@
+const int N = 1e5 + 10;
+vector<pii> adj[N];
+bool vis[N] = {0}; int d[N];
+void dijkstra(int v, int n){
+	fill(d, d+n, inf); d[v] = 0;
+	priority_queue<pii, vector<pii>, 
+							greater<pii>> pq;
+	pq.push(mp(d[v], v));
+	while(!pq.empty()){
+		int u = pq.top().ss; pq.pop();
+		if(vis[u]) continue; vis[u] = true;
+		for(auto &it: adj[u]) {
+			if(d[it.ff] > d[u] + it.ss) {
+				d[it.ff] = d[u] + it.ss;
+				pq.push({d[it.ff], it.ff});
+}	} } }
