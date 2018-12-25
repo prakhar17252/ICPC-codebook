585cbb5e71fda3a66d79cf74b58f2b8eea73a193
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-25 16:20:30 +0530

    Added Geometry

--- geom-2d.cpp
+++ geom-2d.cpp
@@ -81,7 +81,7 @@ bool SegmentsIntersect(PT a, PT b, PT c, PT d) {
 //with line passing through c to d, assuming
 //unique intersection exists; for segment
 //intersection, find if segments intersect first
-PT ComputeLineIntersection(PT a, PT b, PT c, PT d) {
+PT ComputeLineIntersection(PT a, PT b, PT c, PT d){
   b = b-a; d = c-d; c = c-a;
   assert(dot(b, b) > EPS && dot(d, d) > EPS);
   return a + b*cross(c, d)/cross(b, d); }
