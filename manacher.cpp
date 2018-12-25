14da2617b82dbfd766966c34eb173c47ce45fe30
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-25 14:04:00 +0530

    Added Manacher

---
+++ manacher.cpp
@@ -0,0 +1,27 @@
+// Maximal palindrome lengths centered around each
+// position in a string(incl. positions between 
+// characters) and returns them in left-to-right 
+// order of centres. Linear time.
+// Ex: "opposes" -> 
+// [0, 1, 0, 1, 4, 1, 0, 1, 0, 1, 0, 3, 0, 1, 0]
+vector<int> fastLongestPalindromes(string str) {
+  int i = 0, j, d, s, e, lLen, palLen=0;
+  vector<int> res;
+  while(i < len(str)) {
+    if(i > palLen && str[i-palLen-1] == str[i]) {
+      palLen += 2; i++; continue;
+    }
+    res.pb(palLen); s = len(res) - 2, e = s-palLen;
+    bool b = true;
+    for(j = s; j > e; j--) {
+      d = j - e - 1;
+      if(res[j] == d){palLen = d; b = 0; break;}
+      res.pb(min(d, res[j]));
+    }
+    if(b){ palLen = 1; i++; }
+  }
+  res.pb(palLen); lLen = len(res);
+  s = lLen-2; e = s-(2 * len(str) + 1 - lLen);
+  for(i = s; i > e; i--) { 
+    d = i-e-1; res.pb(min(d, res[i])); }
+  return res;}
