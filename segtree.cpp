e0f1220bb25cad2a7f6359ad80f41f7477fbee7f
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-25 15:11:36 +0530

    Added Segment Tree with Lazy propogation

---
+++ segtree.cpp
@@ -0,0 +1,64 @@
+const int N = 1e5 + 10;
+int seg[4*N], lz[4*N], a[N], n;
+
+#define lc 2*cur
+#define rc 2*cur+1
+#define mid (s+e)/2
+
+inline int merge(int x, int y) { return x + y; }
+inline int cnt(int l, int r) { return r - l + 1; }
+// return 1 for RMQ
+
+void build(int cur, int s, int e) {
+	if(s == e) {
+    seg[cur] = a[s];
+    return;
+  }
+  build(lc, s, mid); build(rc, mid+1, e);
+  seg[cur] = merge(seg[lc], seg[rc]);
+}
+
+void update(int cur, int s, int e, 
+                            int l, int r, int val){
+  if(s != e){lz[lc] += lz[cur]; lz[rc] += lz[cur];}
+  seg[cur] += cnt(s, e) * lz[cur]; lz[cur] = 0;
+  if(e < l || s > r) return;
+  if(l <= s && e <= r) {
+    seg[cur] += cnt(s, e) * val;
+    if(s != e) { lz[lc] += val; lz[rc] += val; }
+    return;
+  }
+  update(lc, s, mid, l, r, val);
+  update(rc, mid+1, e, l, r, val);
+  seg[cur] = merge(seg[lc], seg[rc]);
+}
+
+int query(int cur, int s, int e, int l, int r) {
+  if(s != e){lz[lc] += lz[cur]; lz[rc] += lz[cur];}
+  seg[cur] += cnt(s, e) * lz[cur]; lz[cur] = 0;
+  if(e < l || s > r) return 0; // -inf for RMQ
+  if(l <= s && e <= r) return seg[cur];
+
+  int q1 = query(lc, s, mid, l, r);
+  int q2 = query(rc, mid+1, e, l, r);
+  return merge(q1, q2);
+}
+
+void build() { build(1, 1, n); }
+void update(int pos, int val) 
+{ update(1, 1, n, pos, pos, val); }
+void update(int l, int r, int val) 
+{ update(1, 1, n, l, r, val); }
+int query(int l, int r) 
+{ return query(1, 1, n, l, r); }
+
+// For merge sort tree in complete overlap in query
+// Replace lower and upper to remove equality sign
+/*    Number of elements <= k in [l, r]
+  upper_bound(all(seg[cur]), k)-seg[cur].begin();
+ 
+      Number of elements >= k in [l, r]
+  seg[cur].end()-lower_bound(all(seg[cur]), k);
+*/
+// merge(all(seg[lc]), all(seg[rc]), 
+//                       back_inserter(seg[cur]));
