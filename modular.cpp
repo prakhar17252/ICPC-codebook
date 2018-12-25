b17a6018995b4a698c0b528fcbaada55ee9bcb6a
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-25 21:36:03 +0530

    Added Number Theory Functions

--- modular.cpp
+++ modular.cpp
@@ -5,26 +5,24 @@
 
 // computes lcm(a,b)
 int lcm(int a, int b) {
-  return (a / gcd(a,b)) * b;
+  return (a / __gcd(a,b)) * b;
 }
 
 // returns gcd(a,b); finds x,y s.t. d = ax + by
 int extended_euclid(int a, int b, int &x, int &y) {
-  int xx = y = 0;
-  int yy = x = 1;
+  int xx = y = 0; int yy = x = 1;
   while (b) {
     int q = a/b;
     int t = b; b = a%b; a = t;
     t = xx; xx = x-q*xx; x = t;
     t = yy; yy = y-q*yy; y = t;
-  }
-  return a;
+  } return a;
 }
 
 // finds all solutions to ax = b (mod n)
 vll modular_equation_solver(int a, int b, int n){
-	vll solutions;
-	pii x, y, d = extended_euclid(a, n, x, y);
+	vll solutions; int x, y;
+	int d = extended_euclid(a, n, x, y);
 	if (!(b%d)) {
 		x = takemod (x*(b/d), n);
 		for (int i = 0; i < d; i++)
@@ -34,7 +32,7 @@ vll modular_equation_solver(int a, int b, int n){
 
 // computes a^-1 (mod n), returns -1 on failure
 int mod_inverse(int a, int n) {
-	int x, y, d = extended_euclid(a, n, x, y);
+	int x, y; int d = extended_euclid(a, n, x, y);
 	if (d > 1) return -1;
 	return takemod(x,n);
 }
@@ -44,7 +42,7 @@ int mod_inverse(int a, int n) {
 // unique modulo M = lcm(x,y).
 // Return (z,M). On failure, M = -1.
 pii CRT(int x, int a, int y, int b) {
-  int s, t, d = extended_euclid(x, y, s, t);
+  int s, t; int d = extended_euclid(x, y, s, t);
   if (a%d != b%d) return mp(0, -1);
   return mp(takemod(s*b*x+t*a*y,x*y)/d, x*y/d);
 }
@@ -69,8 +67,9 @@ pii CRT(const vll &x, const vll &a) {
 // solutions of equation where k is any integer
 bool linear_diophantine(int a, int b, int c, 
 											int &x, int &y, int &g) {
-	g = gcd(a,b);
+	g = __gcd(a,b);
 	if (c%g) return false;
 	x = c/g * mod_inverse(a/g, b/g); y = (c-a*x)/b;
 	assert(c == a * x + b * y);
-	return true; }
+	return true; 
+}
