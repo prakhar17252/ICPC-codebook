784e248a6f0c2ab0cb1b8d2d875c9421626e784d
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-26 18:42:11 +0530

    Merged KMP with Z and Prefix Function

--- Z&PF.cpp
+++ Z&PF.cpp
@@ -2,8 +2,7 @@ const int N = 1e5 + 10;
 int z[N], p[N];
 
 void zfunction(string s) {
-	int l = 0, r = 0, n = len(s);
-	z[0] = 0;
+	int l = 0, r = 0, n = len(s); z[0] = 0;
 	for(int i = 1; i < n; i++) {
 		if(i <= r) z[i] = min(z[i-l], r - i + 1);
 		
@@ -15,11 +14,26 @@ void zfunction(string s) {
 } } }
 
 void prefixfunction(string s) {
-	p[0] = 0;
+	p[0] = 0; int n = len(s);
 	for(int i = 1; i < n; i++) {
 		int j = p[i-1];
 		while(j > 0 && s[j] != s[i]) j = p[j-1];
-		if(s[j] == s[i]) j++;						// emaxx
-		p[i] = j;												// emaxx
-		// if(s[j] == s[i]) p[i] = j+1;	// Mike notes
+		if(s[j] == s[i]) p[i] = j+1;
 } }
+
+// Returns all positions where match is found
+vector<int> kmpsearch(string text, string pat){
+	int n = len(text), m = len(pat), i = 0, j = 0;
+	prefixfunction(pat); vector<int> res;
+	while(i < n) {
+		if(pat[j] == text[i]){
+			j++; i++;
+		} if(j == m) {
+			res.pb(i-j);
+			j = p[j-1];
+		} else if (i < n && pat[j] != text[i]){
+			if(j!= 0) j = p[j-1];
+			else i++;
+		}
+	} return res;
+}
