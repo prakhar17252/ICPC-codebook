3d557d7b65d669bf7284199b3d97616e916a8f2c
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-27 18:15:12 +0530

    Added Stable Marriage

--- stablemp.cpp
+++ stablemp.cpp
@@ -1,24 +1,46 @@
-// Gale-Shapley algorithm for stable marriage
-// madj[i][j] is jth highest ranked woman for man i
-// fpref[i][j] is the rank woman i assigns to man j
-// Returns a pair of vectors (mpart, fpart), where
-// mpart[i] gives partner of man i & fpart of woman
-vector<vll> madj, fpref;
-pair<vll, vll> stable_marriage() {
-	int n = madj.size();
-	vll mpart(n, -1), fpart(n, -1), midx(n);
-	queue<int> mfree;
-	for (int i = 0; i < n; i++) mfree.push(i);
-	while (!mfree.empty()) {
-		int m = mfree.front(); mfree.pop();
-		int f = madj[m][midx[m]++];
-		if (fpart[f] == -1) {
-			mpart[m] = f; fpart[f] = m;
-		} else if (fpref[f][m] < fpref[f][fpart[f]]) {
-			mpart[fpart[f]] = -1; mfree.push(fpart[f]);
-			mpart[m] = f; fpart[f] = m;
-		} else {
-			mfree.push(m);
-		}
-	}	return mp(mpart, fpart);
+vector<queue<int>> M; vector<vector<int>> F; int n;
+
+vector<pii> SMP() {
+	vector<int> hus(n+1, -1);	queue<int> q;
+	for(int i = 1; i <= n; i++) q.push(i);
+	while(!q.empty()) {
+		int husb = q.front(); q.pop();
+		int wife = M[husb].front(); M[husb].pop();
+		if(hus[wife] == -1) {
+			hus[wife] = husb;
+		} else if(F[wife][hus[wife]] > F[wife][husb]) {
+			swap(hus[wife], husb); q.push(husb);
+		} else q.push(husb);
+	}
+	vector<pii> res;
+	for(int i = 1; i <= n; i++)	{
+		res.pb(mp(hus[i], i));
+	} return res;
+}
+
+int main() {
+	// n is number of marriages
+	cin >> n; M.resize(n+1); F.resize(n+1);
+	for(int i = 0; i < n; i++) {
+		// First line is the woman in question
+		int r; cin >> r;
+		vector<int> cur(n+1);
+		// Next n elements is husbands listed in rank
+		// Earlier is better
+		for(int j = 0; j < n; j++) {
+			int x; cin >> x;
+			cur[x] = j;
+		} F[r] = cur;
+	}
+	// Same for men as women
+	for(int i = 0; i < n; i++) {
+		int r; cin >> r;
+		queue<int> cur;
+		for(int j = 0; j < n; j++) {
+			int x; cin >> x;
+			cur.push(x);
+		} M[r] = cur;
+	}
+	auto ans = SMP();
+	cout << ans << endl;
 }
