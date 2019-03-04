ed2e75b29685c7216a525dac4373ac5c84e89983
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2019-03-05 00:42:53 +0530

    Update yarin.cpp

--- yarin.cpp
+++ yarin.cpp
@@ -4,13 +4,12 @@
 #define MAXSIEVEHALF (MAXSIEVE/2)
 #define MAXSQRT 5000 // sqrt(MAXSIEVE)/2
 char a[MAXSIEVE/16+2];
-#define isprime(n) (a[(n)>>4]&(1<<(((n)>>1)&7)))
-// Works when n is odd
+bool isprime(int n)
+{return n==2 || (n&1 && a[n>>4]&(1<<((n>>1)&7))); }
 
-int i, j;
 memset(a, 255, sizeof(a));
 a[0] = 0xFE;
-for(i = 1; i < MAXSQRT; i++)
+for(int i = 1; i < MAXSQRT; i++)
 	if(a[i >> 3] && (1 << (i & 7)))
-		for(j = 3*i+1; j<MAXSIEVEHALF; j += 2*i+1)
+		for(int j = 3*i+1; j<MAXSIEVEHALF; j += 2*i+1)
 			a[j>>3] &= ~(1 << (j & 7));
