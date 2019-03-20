133cccb7a9f0ea78771822f7e3227c84778fbae4
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2019-03-20 19:31:13 +0530

    Update PushRelabel.cpp

--- PushRelabel.cpp
+++ PushRelabel.cpp
@@ -1,26 +1,26 @@
 // Initialise with no. of vertices
-// To get flows, look at edges with non zero flow
-// max_flow(S, T) to get max flow from S to T
+// To get flows, look at edges with positive f in g
+// Edge e in g[i] goes from i to e.to
 // Runtime:
 // Push relabel in O(V^2 E^0.5) with gap heuristic
+using T = long long; T INF = 1e18;
+struct Edge { int to, rev; T f, c; };
 
 struct MaxFlow {
-  using T = long long; T INF = 1e18;
-  struct Edge { int to, from; T f, c; };
-  
-  vector<vector<Edge>> g; vector<T> ec; vll H;
+  vector<vector<Edge>> g; vector<T> ec;
   vector<Edge*> cur; vector<vector<int>> hs;
+  vector<int> H;
 
   MaxFlow(int n) : g(n),ec(n),cur(n),hs(2*n),H(n){}
 
   void add_edge(int s, int t, T cap, T rcap = 0) {
     if (s == t) return;
     g[s].push_back({t, len(g[t]), 0, cap});
-    g[t].push_back({s, len(g[s]), 0, rcap});
+    g[t].push_back({s, len(g[s])-1, 0, rcap});
   }
 
   void add_flow(Edge& e, T f) {
-    Edge &back = g[e.to][e.from];
+    Edge &back = g[e.to][e.rev];
     if (!ec[e.to] && f) hs[H[e.to]].pb(e.to);
     e.c -= f; ec[back.to] -= f; back.f -= f;
     e.f += f; ec[e.to] += f; back.c += f; 
@@ -52,7 +52,7 @@ struct MaxFlow {
             add_flow(*cur[u],min(ec[u],cur[u]->c));         
           else ++cur[u];
         }
-        while (hi>=0 && hs[hi].empty()) --hi;
+        while (hi >= 0 && hs[hi].empty()) --hi;
     } } return -ec[s]; 
   }
 };
