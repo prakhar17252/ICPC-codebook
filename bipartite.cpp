7c747c83cc6a73d2d0cc67b700177064d4ac94ba
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2019-03-23 01:42:27 +0530

    Added self written Hopcroft Karp implementation

--- bipartite.cpp
+++ bipartite.cpp
@@ -1,35 +1,37 @@
-// Returns maximum bipartite matching
 // The match can be recovered using the vector L
-// Works with 0 based too. Very Fast in practice
+// Works with 1 based only. Very Fast in practice.
 // init with n, m -> sz of grp 1 & 2 (bipartite)
-// addEdge(x,y) -> edge bet x of grp1, y of grp2
+// addEdge(x, y) -> edge b/w x of grp1, y of grp2
 struct BipartiteMatcher {
-  vector<vll> G; vll L, R, Viz;
-  
-  BipartiteMatcher(int n, int m) :
-  G(n), L(n, -1), R(m, -1), Viz(n) {}
-  
-  void AddEdge(int a, int b) { G[a].pb(b); }
-  
-  bool Match(int node) {
-    if(Viz[node]) return false;
-    Viz[node] = true;
-    for(auto vec: G[node]) {
-      if(R[vec] == -1) {
-        L[node] = vec; R[vec] = node; return true;
-    } }
-    for(auto vec: G[node]) {
-      if(Match(R[vec])) {
-        L[node] = vec; R[vec] = node; return true;
-		} } return false; }
-  
-  int GetMaximumMatching() {
-    int ok = 1, ret = 0;
-    while (ok--) { fill(all(Viz), 0);
-      for(int i = 0; i < len(L); ++i)
-        if(L[i] == -1) ok |= Match(i);
+  vector<vll> graph; vll L, R, dist;
+  const int NIL = 0; int n, m;
+  BipartiteMatcher(int n, int m) : n(n), m(m), 
+          graph(n+1), L(n+1), R(m+1), dist(n+1) {}
+
+  void add_edge(int u, int v) { graph[u].pb(v); }
+  bool BFS() {
+    queue<int> q; dist[NIL] = inf;
+    for (int i = 1; i <= n; i++) {
+      if (L[i] == NIL) { dist[i] = 0; q.push(i); }
+      else dist[i] = inf;
     }
-    for(int i = 0; i < len(L); ++i) 
-			ret += (L[i] != -1);
-    return ret; }
+    while (!q.empty()) {
+      int u = q.front(); q.pop();
+      if (dist[u] >= dist[NIL]) continue;
+      for (int v : graph[u]) if (dist[R[v]] == inf)
+      { dist[R[v]] = dist[u]+1; q.push(R[v]); }
+    } return dist[NIL] != inf; }
+
+  bool DFS(int u) {
+    if (u == NIL) return true;
+    for (int v : graph[u])
+      if (dist[R[v]] == dist[u] + 1 && DFS(R[v]))
+      { L[u] = v; R[v] = u; return true; }
+    dist[u] = inf; return false; }
+
+  int maximum_matching() { // fill L, R with NIL
+    int mat = 0; fill(all(L),NIL),fill(all(R),NIL);
+    while (BFS()) for (int i = 1; i <= n; i++)
+      if (L[i] == NIL && DFS(i)) mat++;
+    return mat; }
 };
