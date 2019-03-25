9fc17e8b64fcb8ce3e17b7d8d134af4f5efdf64d
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2019-03-26 00:27:12 +0530

    Update Z and Prefix Function

--- Z&PF.cpp
+++ Z&PF.cpp
@@ -1,18 +1,16 @@
 const int N = 1e5 + 10;
 int z[N], p[N];
 
+//z[i] : longest common prefix(lcp) of s and s[i..]
 void zfunction(string s) {
 	int l = 0, r = 0, n = len(s); z[0] = 0;
-	for(int i = 1; i < n; i++) {
-		if(i <= r) z[i] = min(z[i-l], r - i + 1);
-		
-		while(z[i]+i < n && s[z[i]] == s[z[i]+i])
-			z[i]++;
-		
-		if(z[i] + i - 1 > r) {
-			l = i; r = z[i] + i - 1;
-} } }
+	for (int i = 1; i < n; i++) {
+		if (i <= r) z[i] = min(z[i-l], r - i + 1);
+		while(z[i]+i < n && s[z[i]]==s[z[i]+i])	z[i]++;
+		if (z[i] + i - 1 > r) { l = i; r = z[i]+i-1; } 
+} }
 
+//p[i] : pref of s[0...i] that is also its suffix
 void prefixfunction(string s) {
 	p[0] = 0; int n = len(s);
 	for(int i = 1; i < n; i++) {
@@ -22,18 +20,14 @@ void prefixfunction(string s) {
 } }
 
 // Returns all positions where match is found
-vector<int> kmpsearch(string text, string pat){
+vector<int> kmpsearch(string text, string pat) {
 	int n = len(text), m = len(pat), i = 0, j = 0;
 	prefixfunction(pat); vector<int> res;
-	while(i < n) {
-		if(pat[j] == text[i]){
-			j++; i++;
-		} 
-		if(j == m) {
-			res.pb(i-j); j = p[j-1];
-		} else if (i < n && pat[j] != text[i]){
-			if(j!= 0) j = p[j-1];
+	while (i < n) {
+		if (pat[j] == text[i]) { j++; i++; } 
+		if (j == m) { res.pb(i - j); j = p[j-1]; } 
+    else if (i < n && pat[j] != text[i]) {
+			if (j != 0) j = p[j-1];
 			else i++;
-		}
-	} return res;
+  } } return res;
 }
