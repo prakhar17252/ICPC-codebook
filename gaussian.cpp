16968d863a5d12f28ca95f8cb71451e1e40e4084
Prakhar Gupta <prakhar17252@iiitd.ac.in>
2019-07-28 16:23:59 +0530

    Formatting Fix

--- gaussian.cpp
+++ gaussian.cpp
@@ -21,7 +21,6 @@ struct F {
     int g = __gcd(n, d); n /= g; d /= g;
     if (d < 0) n = -n, d = -d; return *this; }
   F invert() { return F(d, n).simplify(); }
-  ld abs() { return fabs(1.0 * n / d); }
   F operator - () { return F(-d, n).simplify(); }
   F operator + (const F &o) const // Add -, *, /
   { return F(n*o.d + d*o.n, d*o.d).simplify(); }
@@ -32,13 +31,14 @@ struct F {
   F& operator += (const F &o) // Add -=, *=, /=
   { return *this = *this + o; }
 };
+ld fabs(F& f) { return fabsl(1.0l * f.n / f.d); }
 ostream& operator << (ostream& os, const F &f)
 { return os << "(" << f.n <<"/"<< f.d << ")"; }
 
 const ld EPS = 1e-10; using T = Fraction;
 using VT = vector<T>; using VVT = vector<VT>;
 
-T GaussJordan(VVT &a, VVT &b) { 
+T GaussJordan(VVT &a, VVT &b) {
   int n = a.size(), m = b[0].size(), q;
   vll irow(n), icol(n), ipiv(n); T det = T(1);
 
@@ -48,11 +48,10 @@ T GaussJordan(VVT &a, VVT &b) {
       for(int k = 0; k < n; k++) if(!ipiv[k])
         if(pj==-1 || fabs(a[j][k])>fabs(a[pj][pk]))
         { pj = j; pk = k; }
-    // change fabs for Fraction
     if(fabs(a[pj][pk])<EPS) return T(0); //singular
     ipiv[pk]++;
     swap(a[pj], a[pk]); swap(b[pj], b[pk]);
-    
+
     if(pj != pk) det *= T(-1);
     irow[i] = pj; icol[i] = pk;
 
@@ -63,11 +62,11 @@ T GaussJordan(VVT &a, VVT &b) {
     for(int p = 0; p < n; p++) if(p != pk) {
       c = a[p][pk]; a[p][pk] = T(0);
       for(q = 0; q < n; q++) a[p][q] -= a[pk][q]*c;
-      for(q = 0; q < m; q++) b[p][q] -= b[pk][q]*c;      
+      for(q = 0; q < m; q++) b[p][q] -= b[pk][q]*c;
   } }
 
-  for(int p = n-1; p >= 0; p--) 
-    if(irow[p] != icol[p]) 
-      for(int k = 0; k < n; k++) 
+  for(int p = n-1; p >= 0; p--)
+    if(irow[p] != icol[p])
+      for(int k = 0; k < n; k++)
         swap(a[k][irow[p]], a[k][icol[p]]);
   return det; }
